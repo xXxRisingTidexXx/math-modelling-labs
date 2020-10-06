d55fe6758af581c7b81e9cbfe495b7d5363db0b3
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-10-07 01:21:49 +0300

    Added CLI

--- mathmodel/spatial.py
+++ mathmodel/spatial.py
@@ -1,14 +1,14 @@
-from numpy import hstack, stack, sum, sqrt, full, reshape, append
-from plotly.graph_objs import Scatter, Scatter3d
-from plotly.subplots import make_subplots
+from argparse import ArgumentParser
+from numpy import hstack, stack, sum, sqrt, full, reshape, append, ndarray
+from plotly.graph_objs import Scatter, Scatter3d, Mesh3d, Figure
 from cv2 import (
     imread, COLOR_BGR2GRAY, RETR_TREE, CHAIN_APPROX_SIMPLE, cvtColor, threshold,
     findContours
 )
 
 
-def main():
-    frame = max(
+def main(graph: str):
+    shape = max(
         findContours(
             threshold(
                 cvtColor(imread('images/shape.png'), COLOR_BGR2GRAY),
@@ -21,12 +21,22 @@ def main():
         )[0],
         key=len
     )
-    frame = reshape(frame, (frame.shape[0], 2))
-    frame = append(frame, frame[:1], axis=0)
+    shape = reshape(shape, (shape.shape[0], 2))
+    shape = append(shape, shape[:1], axis=0)
+    if graph == 'framex':
+        frame_2d(shape)
+    else:
+        print(f'There\'s no graphs with name \'{graph}\'')
+
+
+def frame_2d(shape: ndarray):
     figure = make_subplots(
-        rows=1,
-        cols=3,
-        specs=[[{'type': 'xy'}, {'type': 'scene'}, {'type': 'scene'}]]
+        rows=2,
+        cols=2,
+        specs=[
+            [{'type': 'xy'}, {'type': 'scene'}],
+            [{'type': 'scene'}, {'type': 'scene'}]
+        ]
     )
     figure.append_trace(
         Scatter(
@@ -65,12 +75,25 @@ def main():
             hoverinfo='skip',
             line={'color': 'red'}
         ),
-        1,
-        3
+        2,
+        1
+    )
+    figure.append_trace(
+        Mesh3d(
+            x=frame_sphere[:, 0],
+            y=frame_sphere[:, 1],
+            z=frame_sphere[:, 2],
+            alphahull=1
+        ),
+        2,
+        2
     )
     figure.update_layout(showlegend=False)
     figure.show()
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser(description='Renders interactive spatial graphs')
+    parser.add_argument('-g', default='frame-2d', help='graph name')
+    args = parser.parse_args()
+    main(args.g)
