755b636809ece4ca6c0289463f5d68c2c15adcb7
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2020-10-07 02:38:14 +0300

    Applied correct graphing

--- mathmodel/spatial.py
+++ mathmodel/spatial.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from numpy import hstack, stack, sum, sqrt, full, reshape, append, ndarray
+from numpy import sqrt, full, reshape, append, ndarray
 from plotly.graph_objs import Scatter, Scatter3d, Mesh3d, Figure
 from cv2 import (
     imread, COLOR_BGR2GRAY, RETR_TREE, CHAIN_APPROX_SIMPLE, cvtColor, threshold,
@@ -7,8 +7,82 @@ from cv2 import (
 )
 
 
-def main(graph: str):
-    shape = max(
+def frame_plane_2d(shape: ndarray):
+    figure = Figure()
+    figure.add_trace(
+        Scatter(
+            x=shape[:, 0],
+            y=shape[:, 1],
+            mode='lines',
+            hoverinfo='skip',
+            line={'color': 'red'}
+        )
+    )
+    figure.update_yaxes(scaleanchor='x', scaleratio=1)
+    figure.show()
+
+
+def frame_plane_3d(shape: ndarray):
+    figure = Figure()
+    figure.add_trace(
+        Scatter3d(
+            x=shape[:, 0],
+            y=shape[:, 1],
+            z=full((shape.shape[0],), 20),
+            mode='lines',
+            hoverinfo='skip',
+            line={'color': 'red'}
+        )
+    )
+    figure.show()
+
+
+def frame_sphere(shape: ndarray):
+    r = 50
+    x, y, z = shape[:, 0], shape[:, 1], full((shape.shape[0],), 20)
+    k = r / sqrt(x ** 2 + y ** 2 + (z + r) ** 2)
+    figure = Figure()
+    figure.add_trace(
+        Scatter3d(
+            x=x * k,
+            y=y * k,
+            z=z * k,
+            mode='lines',
+            hoverinfo='skip',
+            line={'color': 'red'}
+        )
+    )
+    figure.show()
+
+
+def surface_sphere(shape: ndarray):
+    figure = Figure()
+    figure.add_trace(
+        Mesh3d(
+            x=shape[:, 0],
+            y=shape[:, 1],
+            z=shape[:, 2],
+            alphahull=1
+        )
+    )
+    figure.show()
+
+
+def no_graph(_: ndarray):
+    print(f'There\'s no graphs with such a name')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Renders interactive spatial graphs')
+    parser.add_argument('-g', default='frame-plane-2d', help='graph name')
+    args = parser.parse_args()
+    graphs = {
+        'frame-plane-2d': frame_plane_2d,
+        'frame-plane-3d': frame_plane_3d,
+        'frame-sphere': frame_sphere,
+        'surface-sphere': surface_sphere
+    }
+    points = max(
         findContours(
             threshold(
                 cvtColor(imread('images/shape.png'), COLOR_BGR2GRAY),
@@ -21,79 +95,5 @@ def main(graph: str):
         )[0],
         key=len
     )
-    shape = reshape(shape, (shape.shape[0], 2))
-    shape = append(shape, shape[:1], axis=0)
-    if graph == 'framex':
-        frame_2d(shape)
-    else:
-        print(f'There\'s no graphs with name \'{graph}\'')
-
-
-def frame_2d(shape: ndarray):
-    figure = make_subplots(
-        rows=2,
-        cols=2,
-        specs=[
-            [{'type': 'xy'}, {'type': 'scene'}],
-            [{'type': 'scene'}, {'type': 'scene'}]
-        ]
-    )
-    figure.append_trace(
-        Scatter(
-            x=frame[:, 0],
-            y=frame[:, 1],
-            mode='lines',
-            hoverinfo='skip',
-            line={'color': 'red'}
-        ),
-        1,
-        1
-    )
-    z = 5
-    frame3d = hstack((frame, full((frame.shape[0], 1), z)))
-    figure.append_trace(
-        Scatter3d(
-            x=frame3d[:, 0],
-            y=frame3d[:, 1],
-            z=frame3d[:, 2],
-            mode='lines',
-            hoverinfo='skip',
-            line={'color': 'red'}
-        ),
-        1,
-        2
-    )
-    r = 50
-    k = r / sqrt(sum(hstack((frame, full((frame.shape[0], 1), z + r))) ** 2, axis=1))
-    frame_sphere = frame3d * stack((k, k, k), axis=1)
-    figure.append_trace(
-        Scatter3d(
-            x=frame_sphere[:, 0],
-            y=frame_sphere[:, 1],
-            z=frame_sphere[:, 2],
-            mode='lines',
-            hoverinfo='skip',
-            line={'color': 'red'}
-        ),
-        2,
-        1
-    )
-    figure.append_trace(
-        Mesh3d(
-            x=frame_sphere[:, 0],
-            y=frame_sphere[:, 1],
-            z=frame_sphere[:, 2],
-            alphahull=1
-        ),
-        2,
-        2
-    )
-    figure.update_layout(showlegend=False)
-    figure.show()
-
-
-if __name__ == '__main__':
-    parser = ArgumentParser(description='Renders interactive spatial graphs')
-    parser.add_argument('-g', default='frame-2d', help='graph name')
-    args = parser.parse_args()
-    main(args.g)
+    points = reshape(points, (points.shape[0], 2))
+    graphs.get(args.g, no_graph)(append(points, points[:1], axis=0))
